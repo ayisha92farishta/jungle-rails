6a70c5afefd7c8a990994d0da7a767ddbb261654
ayisha92farishta <ayisha_farishta@yahoo.com>
2022-01-17 16:37:18 +0000

    Add basic authentication to admin/products page

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,7 +1,12 @@
 class Admin::ProductsController < ApplicationController
+  http_basic_authenticate_with name: "Jungle", password: "book"
 
   def index
     @products = Product.order(id: :desc).all
+  end
+
+  def edit
+    
   end
 
   def new
