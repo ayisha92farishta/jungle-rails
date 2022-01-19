e01e131021441486f02ddd79a589dfc499721048
ayisha92farishta <ayisha_farishta@yahoo.com>
2022-01-19 18:14:46 +0000

    Add instance variables

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,12 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['BASIC_AUTH_USERNAME'], password: ENV['BASIC_AUTH_PASSWORD']
-  
+
+
+
   def show
-  end
+    @total_products = Product.all
+    @product_count = Product.all.count
+    @product_sum = Product.sum(:quantity)
+    @catagory_count = Category.all.count
+    end
 end
