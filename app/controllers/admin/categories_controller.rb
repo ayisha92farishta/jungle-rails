1607694cb35819fc7a9a8785c9e789d31c154ebf
ayisha92farishta <ayisha_farishta@yahoo.com>
2022-01-24 17:43:56 +0000

    Poppulate index.erb file and add routes

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,7 +2,7 @@ class Admin::CategoriesController < ApplicationController
   http_basic_authenticate_with name: ENV['BASIC_AUTH_USERNAME'], password: ENV['BASIC_AUTH_PASSWORD']
 
   def index
-    @category = Categories.order(id: :desc).all
+    @categories = Categories.order(id: :desc).all
   end
 
   def new
