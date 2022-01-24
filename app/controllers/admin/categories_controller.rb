b7c77e93db2274afd4913adabf2eba6a2d2841d9
ayisha92farishta <ayisha_farishta@yahoo.com>
2022-01-24 17:39:22 +0000

    Poppulate controller file

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,23 @@
 class Admin::CategoriesController < ApplicationController
   http_basic_authenticate_with name: ENV['BASIC_AUTH_USERNAME'], password: ENV['BASIC_AUTH_PASSWORD']
 
+  def index
+    @category = Categories.order(id: :desc).all
+  end
+
+  def new
+    @category = Categories.new
+  end
+
+  def create
+    @category = Categories.new(product_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
 
 end
