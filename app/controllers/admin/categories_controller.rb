6a6ec96070b6a3b4930d1d509a24d4e46b2258c5
ayisha92farishta <ayisha_farishta@yahoo.com>
2022-01-24 18:06:41 +0000

    Render all categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,15 +2,15 @@ class Admin::CategoriesController < ApplicationController
   http_basic_authenticate_with name: ENV['BASIC_AUTH_USERNAME'], password: ENV['BASIC_AUTH_PASSWORD']
 
   def index
-    @categories = Categories.order(id: :desc).all
+    @categories = Category.order(id: :desc).all
   end
 
   def new
-    @category = Categories.new
+    @category = Category.new
   end
 
   def create
-    @category = Categories.new(product_params)
+    @category = Category.new(product_params)
 
     if @category.save
       redirect_to [:admin, :categories], notice: 'Category created!'
