d7d7855e52e66f6728e22a7474a31a58e017291e
ayisha92farishta <ayisha_farishta@yahoo.com>
2022-01-31 22:40:36 +0000

    Add one more test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'Validations' do
-    it 'user should save with proper validations' do
+    it 'should save with proper validations' do
       @user = User.new(first_name: "Anna", 
       last_name: "Kornikova", email: 'abd@gmail.com', password: "asdqwe", password_confirmation: "asdqwe")
 
@@ -10,5 +10,18 @@ RSpec.describe User, type: :model do
 
       expect(@user.id).to be_present
     end
+
+    it 'should not save if password does not match with password_confirmation'
+
+    it 'should not log in if email already exists by another user'
+
+    it 'should not save if first name is not present'
+    
+    it 'should not save if last name is not present'
+
+    it 'should not save if email is not present'
+
+    it 'should not save due to length of password being to small'
+
   end
 end
