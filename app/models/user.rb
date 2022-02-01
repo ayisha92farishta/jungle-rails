914da0ffecb6bc57161f40cfec8f67ee86dcf828
ayisha92farishta <ayisha_farishta@yahoo.com>
2022-02-01 03:51:04 +0000

    Populate a test for user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
+
+  validates :email, uniqueness :true
+
+  def create
+    if user = Use
+  end
+
+
   has_secure_password
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,27 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'Validations' do
-    it 'should save with proper validations' do
-      @user = User.new(first_name: "Anna", 
-      last_name: "Kornikova", email: 'abd@gmail.com', password: "asdqwe", password_confirmation: "asdqwe")
+  # describe 'Validations' do
+  #   it 'should save with proper attributes' do
+  #     @user = User.new(first_name: "Anna", 
+  #     last_name: "Kornikova", email: 'abd@gmail.com', password: "asdqwe", password_confirmation: "asdqwe")
 
-      @user.save
+  #     @user.save
 
-      expect(@user.id).to be_present
-    end
+  #     expect(@user.id).to be_present
+  #   end
 
-    it 'should not save if password does not match with password_confirmation'
+  #   it 'should not save if password does not match with password_confirmation' do
+  #   @user = User.new(first_name: "Anna", 
+  #   last_name: "Kornikova", email: 'abd@gmail.com', password: "asdqwe", password_confirmation: "qweasd")
 
-    it 'should not log in if email already exists by another user'
+  #   @user.valid?
 
-    it 'should not save if first name is not present'
+  #   expect(@user.errors[:password])
+  #  end
+
+  #   it 'should not log in if email already exists by another user'
+
+  #   it 'should not save if first name is not present'
     
-    it 'should not save if last name is not present'
+  #   it 'should not save if last name is not present'
 
-    it 'should not save if email is not present'
+  #   it 'should not save if email is not present'
 
-    it 'should not save due to length of password being to small'
+  #   it 'should not save due to length of password being to small'
+  # end
 
+  describe '.authenticate_with_credentials' do
+
+    it 'Authenticate' do
+      User.create(:email => 'abd@gmail.com', :password => "asdqwe")
+      @user = User.authenticate_with_credentials(
+        'abd@gmail.com','asdqwe')
+      expect(@user).to be_truthy
+    end
   end
 end
