1fbbc8f0241888f2da21f27ced58185dcce068d7
ayisha92farishta <ayisha_farishta@yahoo.com>
2022-02-01 21:35:52 +0000

    Add more test and pass them

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,9 @@
 class User < ActiveRecord::Base
 
-  validates :email, uniqueness :true
-
-  def create
-    if user = Use
-  end
-
-
   has_secure_password
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password, confirmation: true, length: {minimum: 6 }
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,43 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # describe 'Validations' do
-  #   it 'should save with proper attributes' do
-  #     @user = User.new(first_name: "Anna", 
-  #     last_name: "Kornikova", email: 'abd@gmail.com', password: "asdqwe", password_confirmation: "asdqwe")
+  describe 'Validations' do
+    it 'should save with proper attributes' do
+      @user = User.new(first_name: "Anna", 
+      last_name: "Kornikova", email: 'abd@gmail.com', password: "asdqwe", password_confirmation: "asdqwe")
 
-  #     @user.save
+      @user.save
 
-  #     expect(@user.id).to be_present
-  #   end
+      expect(@user.id).to be_present
+    end
 
-  #   it 'should not save if password does not match with password_confirmation' do
-  #   @user = User.new(first_name: "Anna", 
-  #   last_name: "Kornikova", email: 'abd@gmail.com', password: "asdqwe", password_confirmation: "qweasd")
+    it 'should not save if password does not match with password_confirmation' do
+    @user = User.new(first_name: "Anna", 
+    last_name: "Kornikova", email: 'abd@gmail.com', password: "asdqwe", password_confirmation: "qweasd")
 
-  #   @user.valid?
+    @user.valid?
 
-  #   expect(@user.errors[:password])
-  #  end
+    expect(@user.errors[:password])
+   end
 
-  #   it 'should not log in if email already exists by another user'
+    it 'should not log in if email already exists by another user'
 
-  #   it 'should not save if first name is not present'
+    it 'should not save if first name is not present' do
+      @user = User.new( last_name: "Kornikova", email: 'abd@gmail.com', password: "asdqwe", password_confirmation: "asdqwe")
+
+      @user.valid?
+
+      expect(@user.errors[:first_name])
+    end
     
-  #   it 'should not save if last name is not present'
+    it 'should not save if last name is not present' do
+      @user = User.new(first_name: "Anna", email: 'abd@gmail.com', password: "asdqwe", password_confirmation: "asdqwe")
+      @user.valid?
 
-  #   it 'should not save if email is not present'
+      expect(@user.errors[:last_name])
 
-  #   it 'should not save due to length of password being to small'
-  # end
+    end
+
+    it 'should not save if email is not present'
+
+    it 'should not save due to length of password being to small'
+  end
 
   describe '.authenticate_with_credentials' do
 
-    it 'Authenticate' do
-      User.create(:email => 'abd@gmail.com', :password => "asdqwe")
-      @user = User.authenticate_with_credentials(
-        'abd@gmail.com','asdqwe')
-      expect(@user).to be_truthy
+    it 'should return the user if successfully authenticated' do
+      user1 = User.new(:name => 'abc', :email => 'abc@gmail.com', :password => 'abc@123', :password_confirmation => 'abc@123')
+      user1.save
+      @user = User.authenticate_with_credentials('abc@gmail.com', 'abc@123')
+      expect(@user).to be_present
     end
-  end
+
+  #   it 'Authenticate' 
+  #   # do
+  #   #   User.create(:email => 'abd@gmail.com', :password => "asdqwe")
+  #   #   @user = User.authenticate_with_credentials(
+  #   #     'abd@gmail.com','asdqwe')
+  #   #   expect(@user).to be_truthy
+  #   # end
+  # end
+
+  
 end
